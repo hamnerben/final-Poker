a162996bce833e33702a9d3fc0de5430e81df2ee
SpencerPotter <a02310145@usu.edu>
2022-12-04 16:57:17 -0700

    new upgrade again

--- TodosMVVM/app/src/main/java/com/usu/todosmvvm/UpgradeFragment.kt
+++ TodosMVVM/app/src/main/java/com/usu/todosmvvm/UpgradeFragment.kt
@@ -1,4 +1,16 @@
 package com.usu.todosmvvm
 
-class UpgradeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class UpgradeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?{
+        return binding.root
+    }
 }
