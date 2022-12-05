e8156af83f9b87a5669f0a64fca5efb1b438b036
Spencer McQueen <srmmcqueen@outlook.com>
2022-12-05 16:32:08 -0700

    changed pastry to pastries[0]

--- TodosMVVM/app/src/main/java/com/usu/todosmvvm/PastryViewModel.kt
+++ TodosMVVM/app/src/main/java/com/usu/todosmvvm/PastryViewModel.kt
@@ -29,36 +29,38 @@ class PastryViewModel: ViewModel() {
     }
 
     fun upgradeclick(){
-        if(pastry.pastries >= pastry.clickUpgradeCost) {
-            pastry.clickPower *= 2
-            pastry.autoClickUpgradeCost *= 3
-            pastry.pastries -= pastry.clickUpgradeCost
+        if(pastries[0].pastries >= pastries[0].clickUpgradeCost) {
+            pastries[0].clickPower *= 2
+            pastries[0].autoClickUpgradeCost *= 3
+            pastries[0].pastries -= pastries[0].clickUpgradeCost
         }
     }
 
     fun upgradeOfflineproduction(){
-        if(pastry.pastries >= pastry.offLineProductionUpgradeCost) {
-            pastry.offLineProduction += 5
-            pastry.offLineProductionUpgradeCost *= 3
-            pastry.pastries -= pastry.offLineProductionUpgradeCost
+        if(pastries[0].pastries >= pastries[0].offLineProductionUpgradeCost) {
+            pastries[0].offLineProduction += 5
+            pastries[0].offLineProductionUpgradeCost *= 3
+            pastries[0].pastries -= pastries[0].offLineProductionUpgradeCost
         }
 
     }
 
     fun upgradeAutoClicker(){
-        if(pastry.pastries >= pastry.autoClickUpgradeCost) {
-            pastry.autoClicker += (pastry.autoClickUpgradeCost / 10000)
-            pastry.autoClickUpgradeCost += pastry.autoClickUpgradeCost / 4
-            pastry.pastries -= pastry.autoClickUpgradeCost
+        if(pastries[0].pastries >= pastries[0].autoClickUpgradeCost) {
+            pastries[0].autoClicker += (pastries[0].autoClickUpgradeCost / 10000)
+            pastries[0].autoClickUpgradeCost += pastries[0].autoClickUpgradeCost / 4
+            pastries[0].pastries -= pastries[0].autoClickUpgradeCost
         }
     }
 
     fun getpastries(): Int {
-        return pastry.pastries
+        return pastries[0].pastries
     }
 
     fun getUpgradeClickCost(): Int{
-        return pastry.clickUpgradeCost
+        return pastries[0].clickUpgradeCost
     }
+
+    
 
 }
